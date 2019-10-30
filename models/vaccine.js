7055aad94dec08ec16cee87300c089aecf139e6f
Tyler Walker <tylerjwalker@gmail.com>
2019-10-30 14:50:25 -0600

    changed vax collection with description

--- models/vaccine.js
+++ models/vaccine.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const vaccineSchema = new mongoose.Schema({
     name: {type: String, required: true},
     month: {type: Number, required: true},
-    round: Number
+    round: Number,
+    description: String
 });
 
 const Vaccine = mongoose.model('Vaccine', vaccineSchema);
